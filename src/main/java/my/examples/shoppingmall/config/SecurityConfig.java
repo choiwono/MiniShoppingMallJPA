875edc8d654d93f8148845c5629dff09207ee9b1
choiwono <treasure_b@naver.com>
2019-02-21 16:46:00 +0900

    # 스프링 시큐리티 설정 변경

--- src/main/java/my/examples/shoppingmall/config/SecurityConfig.java
+++ src/main/java/my/examples/shoppingmall/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .antMatchers("/order/writeform").permitAll()
             .antMatchers("/order/directorder").permitAll()
             .antMatchers("/order/complete").permitAll()
+            .antMatchers("/order/record").permitAll()
             .antMatchers("/cart/**").permitAll()
             .antMatchers("/users/join").permitAll()
             .antMatchers("/users/welcome").permitAll()
